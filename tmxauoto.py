108a748d0fc61664a8c902ed970880453bbc88ab
B4yrammllly <109522372+B4yrammllly@users.noreply.github.com>
2022-08-04 17:56:39 +0300

    Add files via upload

---
+++ tmxauoto.py
@@ -0,0 +1,93 @@
+import os
+import time
+print("""
+********************************************************\nB4YRAMLLLY TERMUX OTO TEMEL KURULUM\n********************************************************
+
+""")
+
+
+print("\n\n")
+time.sleep(2)
+
+
+
+
+
+print("\n bu kurulum net olarak 18 dakika 27 saniyede tamamlanacaktır kurulum esnasında terminalden çıkmayıp tv vb şeylerle vakit geçirmeniz bu toolun çalışması için en sağlıklı yoldur kurulum esnasında termuxdan çıkarsanız kurulum tamamlanamayabilir....\n\n")
+time.sleep(2)
+a=input("otomatik kurulumubaşlatmak için entera basınız\n")
+time.sleep(2)
+print("başlatılıyor....")
+time.sleep(1)
+print("başlatılıyor....")
+time.sleep(2)
+print("başlatılıyor....")
+time.sleep(2)
+print("\n\n")
+
+
+
+
+
+
+
+
+print("wget yükleniyor...")
+time.sleep(1)
+os.system("pkg install wget")
+time.sleep(60)
+print("python2  yükleniyor...")
+time.sleep(1)
+os.system( "pkg install python2 ")
+time.sleep(60)
+print("python3  yükleniyor...")
+time.sleep(1)
+os.system("pkg install python3")
+time.sleep(60)
+print("php yükleniyor...")
+time.sleep(1)
+os.system("pkg install php")
+time.sleep(60)
+print("perl yükleniyor...")
+time.sleep(1)
+os.system("pkg install perl ")
+time.sleep(60)
+print("nano yükleniyor...")
+time.sleep(1)
+os.system("pkg install nano ")
+time.sleep(60)
+print("vim yükleniyor...")
+time.sleep(1)
+os.system("pkg install vim" )
+time.sleep(60)
+print("cat  yükleniyor...")
+time.sleep(1)
+os.system("pkg install cat" )
+time.sleep(60)
+print("pip  yükleniyor...")
+time.sleep(1)
+os.system("pkg install pip")
+time.sleep(60)
+print("pip2 yükleniyor...")
+time.sleep(1)
+os.system ("pkg install pip2 ")
+time.sleep(60)
+print("worldlist  yükleniyor...")
+time.sleep(1)
+os.system ("pip install wordlist")
+time.sleep(60)
+print("nmap  yükleniyor...")
+time.sleep(1)
+os.system("pkg install nmap ")
+time.sleep(60)
+print("hydra  yükleniyor...")
+time.sleep(1)
+os.system("pkg install hydra ")
+time.sleep(60)
+print("openssl yükleniyor...")
+time.sleep(1)
+os.system("pkg install openssl ")
+time.sleep(60)
+print("nodejs  yükleniyor...")
+time.sleep(1)
+os.system("apt install nodejs")
